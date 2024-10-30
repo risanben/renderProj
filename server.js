e15d30fe5a39990cf444ab569dfa74ee77ff3465
Risan <risan@Risans-Air.lan>
2024-10-30 16:50:48 +0200

    adding port

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/**', (req, res) => {
 })
 
 // Listen will always be the last line in our server!
-const port = 3030
+const port = process.env.PORT || 3030
 app.listen(port, () => {
   loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
 })
